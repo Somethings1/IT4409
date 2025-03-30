9b60d1639238ba799cfe651a729cd5ea13d96250
longtran22 <baolong081104@gmail.com>
2025-03-30 17:27:20 +0700

    init welcom page

---
+++ frontend/src/components/introduce/Main_intro.jsx
@@ -0,0 +1,32 @@
+
+import './main.css'
+
+
+
+function Main(){
+
+   return(<>
+ 
+   <div    className="main">
+    <header>
+        <div className="logo"><img style={{height:"112px",position:"absolute",top:"0px",left:"33%"}} />LEET CODE</div>
+        <div className="auth-buttons">
+            <button className="btn"
+         
+            >Đăng nhập</button>
+            <button className="btn"
+           
+            >Đăng ký</button>
+        </div>
+    
+    </header>
+{
+    <section className="content" style={{ color: "white"}}>
+
+        <p>Chào mừng đến với trang web của chúng tôi! Đây là nơi giới thiệu các tính năng và dịch vụ mà chúng tôi cung cấp.</p>
+    </section> }
+   
+    </div>
+    </>) 
+}
+export default Main
