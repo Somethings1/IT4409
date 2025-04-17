b4bafb7899f656686b9dea1f87d04606949de196
Nguyen Hoang Long <hoanglong1292004@gmail.com>
2025-04-17 22:01:20 +0700

    create first admin acc

--- backend/leetcode/src/main/java/com/example/leetcode/controller/UserController.java
+++ backend/leetcode/src/main/java/com/example/leetcode/controller/UserController.java
@@ -2,12 +2,15 @@ package com.example.leetcode.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.leetcode.domain.User;
 import com.example.leetcode.service.UserService;
+import com.example.leetcode.service.error.IdInvalidException;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,35 +34,40 @@ public class UserController {
 		this.passwordEncoder = passwordEncoder;
 	}
 
-	@PostMapping("/user")
-	public User createNewUser(@RequestBody User postManUser) {
-
+	@PostMapping("/users")
+	public ResponseEntity<User> createNewUser(@RequestBody User postManUser) {
+		String hashPassword = this.passwordEncoder.encode(postManUser.getPassword());
+		postManUser.setPassword(hashPassword);
 		User newUser = this.userService.handleCreateUser(postManUser);
-		return newUser;
+		return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
 	}
 
-	@DeleteMapping("/user/{id}")
-	public String deleteUser(@PathVariable("id") long id) {
+	@DeleteMapping("/users/{id}")
+	public ResponseEntity<String> deleteUser(@PathVariable("id") long id) throws IdInvalidException {
+		if (id >= 1500) {
+			throw new IdInvalidException("Id isn't bigger than 1500");
+		}
 		this.userService.handleDeleteUserByID(id);
-		return "Delete user";
+		return ResponseEntity.ok("Delete user");
+		// return ResponseEntity.status(HttpStatus.OK).body("Delete user");
 	}
 
-	@GetMapping("/user/{id}")
-	public User getUserByID(@PathVariable("id") long id) {
+	@GetMapping("/users/{id}")
+	public ResponseEntity<User> getUserByID(@PathVariable("id") long id) {
 		User user = this.userService.fetchUserByID(id);
-		return user;
+		return ResponseEntity.status(HttpStatus.OK).body(user);
 	}
 
-	@GetMapping("/user")
-	public List<User> getAllUsers() {
+	@GetMapping("/users")
+	public ResponseEntity<List<User>> getAllUsers() {
 		List<User> users = this.userService.fetchAllUsers();
-		return users;
+		return ResponseEntity.status(HttpStatus.OK).body(users);
 	}
 
-	@PutMapping("/user")
-	public User updateUser(@RequestBody User postManUser) {
-
-		return this.userService.handleUpdateUser(postManUser);
+	@PutMapping("/users")
+	public ResponseEntity<User> updateUser(@RequestBody User postManUser) {
+		User user = this.userService.handleUpdateUser(postManUser);
+		return ResponseEntity.status(HttpStatus.OK).body(user);
 	}
 
 }
