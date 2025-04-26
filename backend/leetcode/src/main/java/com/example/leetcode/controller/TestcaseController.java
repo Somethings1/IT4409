0a37afd7b4722d461e328599abcd81eac287a6fe
Nguyen Hoang Long <hoanglong1292004@gmail.com>
2025-04-26 08:53:11 +0700

    testcase

---
+++ backend/leetcode/src/main/java/com/example/leetcode/controller/TestcaseController.java
@@ -0,0 +1,76 @@
+package com.example.leetcode.controller;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.leetcode.domain.Testcase;
+import com.example.leetcode.domain.response.ResultPaginationDTO;
+import com.example.leetcode.service.TestcaseService;
+import com.example.leetcode.util.annotation.ApiMessage;
+import com.example.leetcode.util.error.IdInvalidException;
+import com.turkraft.springfilter.boot.Filter;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1")
+public class TestcaseController {
+	private final TestcaseService testcaseService;
+
+	@PostMapping("/testcases")
+	@ApiMessage("Create a new testcase")
+	public ResponseEntity<Testcase> createNewTestcase(@Valid @RequestBody Testcase postmanTestcase)
+			throws IdInvalidException {
+		Testcase testcase = this.testcaseService.handleSaveTestcase(postmanTestcase);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(testcase);
+	}
+
+	@PutMapping("/testcases")
+	@ApiMessage("Update a testcase")
+	public ResponseEntity<Testcase> updateTestcase(@Valid @RequestBody Testcase postmanTestcase)
+			throws IdInvalidException {
+		Testcase testcase = this.testcaseService.fetchTestcaseById(postmanTestcase.getId());
+		if (testcase == null) {
+			throw new IdInvalidException("Testcase with ID = " + postmanTestcase.getId() + " does not exist!");
+		}
+
+		testcase.setInput(postmanTestcase.getInput());
+		this.testcaseService.handleSaveTestcase(testcase);
+		return ResponseEntity.ok(testcase);
+	}
+
+	@GetMapping("/testcases")
+	@ApiMessage("Fetch all testcases")
+	public ResponseEntity<ResultPaginationDTO> getAllTestcases(
+			@Filter Specification<Testcase> specification,
+			Pageable pageable) {
+
+		ResultPaginationDTO dto = this.testcaseService.handleFetchAllTestcases(specification, pageable);
+		return ResponseEntity.ok(dto);
+	}
+
+	@DeleteMapping("/testcases/{id}")
+	@ApiMessage("Delete a testcase")
+	public ResponseEntity<Void> deleteTestcase(@PathVariable("id") long id)
+			throws IdInvalidException {
+		Testcase testcase = this.testcaseService.fetchTestcaseById(id);
+		if (testcase == null) {
+			throw new IdInvalidException("Testcase with ID = " + id + " does not exist!");
+		}
+		this.testcaseService.handleDeleteTestcase(id);
+		return ResponseEntity.ok(null);
+	}
+}
---
+++ backend/leetcode/src/main/java/com/example/leetcode/service/TestcaseService.java
@@ -0,0 +1,49 @@
+package com.example.leetcode.service;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import com.example.leetcode.domain.Testcase;
+import com.example.leetcode.domain.response.ResultPaginationDTO;
+import com.example.leetcode.repository.TestcaseRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class TestcaseService {
+	private final TestcaseRepository testcaseRepository;
+
+	public Testcase handleSaveTestcase(Testcase testcase) {
+		return this.testcaseRepository.save(testcase);
+	}
+
+	public Testcase fetchTestcaseById(long id) {
+		Optional<Testcase> optional = this.testcaseRepository.findById(id);
+		return optional.isPresent() ? optional.get() : null;
+	}
+
+	public ResultPaginationDTO handleFetchAllTestcases(Specification<Testcase> specification, Pageable pageable) {
+		Page<Testcase> page = this.testcaseRepository.findAll(specification, pageable);
+		ResultPaginationDTO result = new ResultPaginationDTO();
+		ResultPaginationDTO.Meta meta = new ResultPaginationDTO.Meta();
+
+		meta.setPage(page.getNumber() + 1);
+		meta.setPageSize(page.getSize());
+		meta.setPages(page.getTotalPages());
+		meta.setTotal(page.getTotalElements());
+
+		result.setMeta(meta);
+		result.setResult(page.getContent());
+		return result;
+	}
+
+	public void handleDeleteTestcase(long id) {
+		this.testcaseRepository.deleteById(id);
+	}
+
+}
