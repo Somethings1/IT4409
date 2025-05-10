e3cd4c370e69fe25d75adf86e634f6c58087a9bc
longtran22 <baolong081104@gmail.com>
2025-05-10 13:12:25 +0700

    complete the user dashboard and admin dashboard

--- backend/leetcode/src/test/java/com/example/leetcode/LeetcodeApplicationTests.java
+++ backend/leetcode/src/test/java/com/example/leetcode/LeetcodeApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.leetcode;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class LeetcodeApplicationTests {
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	@Test
 	void contextLoads() {
 	}
-
 }
